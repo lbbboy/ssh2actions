bc1f008f71d7b6611fdd4a741a49a920159845ee
lbbboy <lbbboy@gmail.com>
2025-09-11 21:41:09 +0800

    Update ngrok2actions.sh

--- ngrok2actions.sh
+++ ngrok2actions.sh
@@ -49,6 +49,11 @@ elif [[ -n "$(uname | grep -i Darwin)" ]]; then
     echo 'PermitRootLogin yes' | sudo tee -a /etc/ssh/sshd_config >/dev/null
     sudo launchctl unload /System/Library/LaunchDaemons/ssh.plist
     sudo launchctl load -w /System/Library/LaunchDaemons/ssh.plist
+
+if [ -n "$GITHUB_WORKSPACE" ]; then
+    echo "cd \$GITHUB_WORKSPACE" >> /root/.bashrc
+fi
+
 else
     echo -e "${ERROR} This system is not supported!"
     exit 1
